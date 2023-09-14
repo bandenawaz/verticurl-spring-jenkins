6a52d9a6733eebcfeb18e7b5419098dec44156d9
bandenawaz <bandenawaz.bagwan@gmail.com>
2023-09-14 14:23:38 +0530

    added test code and logger statement

--- src/main/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplication.java
+++ src/main/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplication.java
@@ -1,15 +1,26 @@
 package com.verticurl.jenkins.springjenkinsproject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsProjectApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringApplication.class);
+
+	public void init() {
+		
+		logger.info("Application Started");
+	}
+
 	public static void main(String[] args) {
+
+		logger.info("application executed...");
 		SpringApplication.run(SpringJenkinsProjectApplication.class, args);
 
-		System.out.println("Welcome to Verticurl FLT Programme");
+		//System.out.println("Welcome to Verticurl FLT Programme");
 	}
 
 }
--- src/test/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplicationTests.java
+++ src/test/java/com/verticurl/jenkins/springjenkinsproject/SpringJenkinsProjectApplicationTests.java
@@ -1,13 +1,23 @@
 package com.verticurl.jenkins.springjenkinsproject;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsProjectApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsProjectApplicationTests.class);
 	@Test
 	void contextLoads() {
+
+		logger.info("Testcase executing...");
+		logger.info("Testcase executing second log statement...");
+		
+		assertEquals(true, true);
 	}
 
 }
